d3d89bb39cedcbb6554c500c273c4f138976198a
warwickxd2 <na16992068505@gmail.com>
2026-06-28 13:31:49 -0300

    Delete codigo2 directory

--- codigo2/components/pages/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useProfile } from '@/lib/ProfileContext';
-import MoradorHome from '@/components/dashboard/MoradorHome';
-import AdminDashboard from '@/components/dashboard/AdminDashboard';
-
-export default function Dashboard() {
-  const { activeProfile } = useProfile();
-  return activeProfile === 'admin' ? <AdminDashboard /> : <MoradorHome />;
-}
